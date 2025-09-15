e0510e24425079f19990c980bf76abb54bd7e725
이근만 <miopadrex@gmail.com>
2025-09-15 17:37:07 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,11 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
+	plugins: [react(), tailwindcss(), svgr()],
+	base: "/",
+	resolve: {
+		alias: {
+			"@": "/src",
+		},
+	},
 });
